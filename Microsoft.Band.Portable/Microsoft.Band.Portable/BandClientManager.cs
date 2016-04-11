5382fa48b790fe689245f20d55f39cf555d5ee50
Matthew Leibowitz <mattleibow@live.com>
2016-04-11 21:42:17 +0200

    Modified the portable connection logic to act like Windows  - support background/foreground modes  - throw when trying to connect an already connected client

--- Microsoft.Band.Portable/Microsoft.Band.Portable/BandClientManager.cs
+++ Microsoft.Band.Portable/Microsoft.Band.Portable/BandClientManager.cs
@@ -41,17 +41,26 @@ namespace Microsoft.Band.Portable
         /// Returns a collection of the Band devices that are paired with the current device.
         /// </summary>
         /// <returns>A collection of the paired Bands.</returns>
-        public async Task<IEnumerable<BandDeviceInfo>> GetPairedBandsAsync()
+        public async Task<IEnumerable<BandDeviceInfo>> GetPairedBandsAsync(bool isBackgound)
         {
 #if __ANDROID__
             return NativeBandClientManager.Instance.GetPairedBands().Select(b => new BandDeviceInfo(b));
 #elif __IOS__
             return NativeBandClientManager.Instance.AttachedClients.Select(b => new BandDeviceInfo(b));
 #elif WINDOWS_PHONE_APP
-            return (await NativeBandClientManager.Instance.GetBandsAsync()).Select(b => new BandDeviceInfo(b));
+            return (await NativeBandClientManager.Instance.GetBandsAsync(isBackgound)).Select(b => new BandDeviceInfo(b));
 #else
             return null;
 #endif
+        }
+
+        /// <summary>
+        /// Returns a collection of the Band devices that are paired with the current device.
+        /// </summary>
+        /// <returns>A collection of the paired Bands.</returns>
+        public Task<IEnumerable<BandDeviceInfo>> GetPairedBandsAsync()
+        {
+            return GetPairedBandsAsync(false);
         }
 
         /// <summary>
@@ -64,9 +73,17 @@ namespace Microsoft.Band.Portable
         {
 #if __ANDROID__
             var nativeClient = NativeBandClientManager.Instance.Create(Application.Context, info.Native);
+            if (nativeClient.IsConnected)
+            {
+                throw new BandException($"Aleady connected to Band '{info.Name}'.", BandErrorType.ServiceError);
+            }
             var result = await nativeClient.ConnectTaskAsync() == ConnectionState.Connected;
             return new BandClient(nativeClient);
 #elif __IOS__
+            if (info.Native.IsConnected)
+            {
+                throw new BandException($"Aleady connected to Band '{info.Native.Name}'.");
+            }
             await NativeBandClientManager.Instance.ConnectTaskAsync(info.Native);
             return new BandClient(info.Native);
 #elif WINDOWS_PHONE_APP
