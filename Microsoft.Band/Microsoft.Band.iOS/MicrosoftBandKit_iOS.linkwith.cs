e5edb853c14d6ad4b0c02896666aa857afa51fde
Matthew Leibowitz <mattleibow@live.com>
2016-04-20 23:20:43 +0200

    Updated the native iOS and Android SDKs  - Android + iOS: v1.3.20307.2  - NuGet: v1.3.9

--- Microsoft.Band/Microsoft.Band.iOS/MicrosoftBandKit_iOS.linkwith.cs
+++ Microsoft.Band/Microsoft.Band.iOS/MicrosoftBandKit_iOS.linkwith.cs
@@ -1,7 +1,7 @@
 using ObjCRuntime;
 
 [assembly: LinkWith (
-    "MicrosoftBandKit_iOS-1.3.20217.2.a", 
+    "MicrosoftBandKit_iOS-1.3.20307.2.a", 
 	LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.Simulator64, 
 	ForceLoad = true,
 	Frameworks = "CoreBluetooth")]
