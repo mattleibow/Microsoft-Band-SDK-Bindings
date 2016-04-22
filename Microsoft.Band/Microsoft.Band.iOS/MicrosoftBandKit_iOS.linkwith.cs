70a59f60e702375dd09feb9351b9190f92c7c390
Matthew Leibowitz <mattleibow@live.com>
2016-04-22 15:51:27 -0400

    Updated the iOS version to v1.3.20419.1

--- Microsoft.Band/Microsoft.Band.iOS/MicrosoftBandKit_iOS.linkwith.cs
+++ Microsoft.Band/Microsoft.Band.iOS/MicrosoftBandKit_iOS.linkwith.cs
@@ -1,7 +1,7 @@
 using ObjCRuntime;
 
 [assembly: LinkWith (
-    "MicrosoftBandKit_iOS-1.3.20307.2.a", 
+    "MicrosoftBandKit_iOS-1.3.20419.1.a", 
 	LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.Simulator64, 
 	ForceLoad = true,
 	Frameworks = "CoreBluetooth")]
